875c4ae624584c8664f58db7b2da950f20d2d966
Shubham Mirashi <136989957+Shubham1905s@users.noreply.github.com>
2024-12-14 19:25:26 +0530

    added pages

--- Cert/urls.py
+++ Cert/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from Cert import views
 
 urlpatterns = [
-    path('', views.home, name='Home')
+    path('', views.home, name='home'),
+    path('landingpage', views.landingpage, name='landingpage'),
+    path('profile', views.profile, name='profile')
+    
 ]
--- Cert/views.py
+++ Cert/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def landingpage(request):
+    return render(request, 'landingpage.html')
+
+def profile(request):
+    return render(request, 'profile.html')
