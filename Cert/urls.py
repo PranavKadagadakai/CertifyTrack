1cdcfa5c986fdf00323aee2433285545f34d5de0
PranavKdagadakai <pkdagadakai@gmail.com>
2024-12-15 12:05:12 +0530

    Added draft Dashboards

--- Cert/urls.py
+++ Cert/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from Cert import views
 
 urlpatterns = [
@@ -9,6 +10,7 @@ urlpatterns = [
     # Authentication URLs
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('logout/', views.custom_logout_view, name='logout'),
 
     # Role-specific Dashboard URLs
     path('student_dashboard/', views.student_dashboard, name='student_dashboard'),
