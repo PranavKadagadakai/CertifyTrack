6aa244890cc8e0da1f4ce72a5ba74f1d35d6b2d2
PranavKdagadakai <pkdagadakai@gmail.com>
2024-12-16 19:49:38 +0530

    Added Profile Page

--- Cert/models.py
+++ Cert/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=[('student', 'Student'), ('club', 'Club'), ('mentor', 'Mentor')])
+    role = models.CharField(max_length=20, choices=[('student', 'Student'), ('club', 'Club'), ('mentor', 'Mentor')], default='student')
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.user.username
